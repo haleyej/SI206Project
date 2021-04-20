2422a1b67197f91154e0a29bd2c21002b9ba4094
Sureet Sarau <sureet@umich.edu>
2021-04-20 17:42:43 -0400

    db stuff works now

--- Unemployment_Scraping.py
+++ Unemployment_Scraping.py
@@ -96,34 +96,29 @@ def create_database(database_name):
 
     return (cur, conn)
 
-def fill_database(umemployment_dict, totals_list):
-
-    cur, conn = setUpDatabase('unemployment_rates.db')
+def fill_database(unemployment_dict, totals_list, cur, conn):
 
     #creates weeks table
     cur.execute('''CREATE TABLE IF NOT EXISTS Weeks (id INTEGER PRIMARY KEY, week TEXT UNIQUE)''')
 
     #creates table of unemployment rates
-    cur.execute("CREATE TABLE IF NOT EXISTS unemployment_rates (week INTEGER PRIMARY KEY, initial_nsa_claims INTEGER, total_claims INTEGER)"
+    cur.execute('''CREATE TABLE IF NOT EXISTS unemployment_rates (week INTEGER PRIMARY KEY, initial_nsa_claims INTEGER, total_claims INTEGER)''')
     
 
-    for x in range(len(umemployment_dict.keys())):
-        weeks = list(umemployment_dict.keys())
+    for x in range(len(unemployment_dict.keys())):
+        weeks = list(unemployment_dict.keys())
 
         #creates variables to insert into table
-        week_key = week[x]
-        unemployment_claim_num = umemployment_dict[week_key]
+        week_key = weeks[x]
+        unemployment_claim_num = unemployment_dict[week_key]
         total = totals_list[x]
-        week_num = i + 1
+        week_num = x + 1
 
         #adds information into weeks table
-        cur.execute('''INSERT OR IGNORE INTO Weeks (week) VALUES (?)''', (week_num, week_key))
-        cur.execute('''SELECT id FROM Weeks where week = (?)''', (week_num,))
-
-        week_id = cur.fetchone()[0]
+        cur.execute('''INSERT OR IGNORE INTO Weeks (id, week) VALUES (?, ?)''', (week_num, week_key))
 
         #adds information into unemployment rates table
-        cur.execute('''INSERT OR IGNORE INTO weeks (week, initial_nsa_claims) VALUES (?, ?, ?)''', (week_id, unemployment_claim_num, total))
+        cur.execute('''INSERT OR IGNORE INTO unemployment_rates (week, initial_nsa_claims, total_claims) VALUES (?, ?, ?)''', (week_num, unemployment_claim_num, total))
     
     print("Finished adding data")
     conn.commit()
@@ -140,7 +135,7 @@ def main():
     cumulative_claims_list = get_weekly_cumulative_total()
 
     cur, conn = create_database('unemployment_tables.db')
-    fill_database(weekly_umemployment_claims_dict, cumulative_claims_list)
+    fill_database(weekly_umemployment_claims_dict, cumulative_claims_list, cur, conn)
 
     conn.close()
 
