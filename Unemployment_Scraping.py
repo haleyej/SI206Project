9a80cd9b278143717823280a83d2a09d2af7dd9f
Sureet Sarau <sureet@umich.edu>
2021-04-20 17:26:39 -0400

    database functions have error. will look more into it

--- Unemployment_Scraping.py
+++ Unemployment_Scraping.py
@@ -60,6 +60,21 @@ def get_initial_nsa_claims():
         nsa_list.append(int(num))
 
     return nsa_list
+
+def get_weekly_cumulative_total():
+    #Function gets a weekly cumulative total of initial NSA unemployment claims as weeks go on
+    #Function will return a list of the cumulative total per week 
+    #List returns numbers chronologically from March 2020 to may 2020
+    total = 0 
+
+    cumulative_totals = []
+    claim_per_week = get_initial_nsa_claims()
+    
+    for num in claim_per_week:
+        total += num
+        cumulative_totals.append(total)
+
+    return cumulative_totals
     
 def make_dict():
     #Function calls get_week_dates and get_initial_nsa_claims 
@@ -72,11 +87,63 @@ def make_dict():
 
     return dict(zip(dates, claim_nums))
 
+def create_database(database_name):
+    #Creates a database to store information in
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+database_name)
+    cur = conn.cursor()
+
+    return (cur, conn)
+
+def fill_database(umemployment_dict, totals_list):
+
+    cur, conn = setUpDatabase('unemployment_rates.db')
+
+    #creates weeks table
+    cur.execute('''CREATE TABLE IF NOT EXISTS Weeks (id INTEGER PRIMARY KEY, week TEXT UNIQUE)''')
+
+    #creates table of unemployment rates
+    cur.execute("CREATE TABLE IF NOT EXISTS unemployment_rates (week INTEGER PRIMARY KEY, initial_nsa_claims INTEGER, total_claims INTEGER)"
+    
+
+    for x in range(len(umemployment_dict.keys())):
+        weeks = list(umemployment_dict.keys())
+
+        #creates variables to insert into table
+        week_key = week[x]
+        unemployment_claim_num = umemployment_dict[week_key]
+        total = totals_list[x]
+        week_num = i + 1
+
+        #adds information into weeks table
+        cur.execute('''INSERT OR IGNORE INTO Weeks (week) VALUES (?)''', (week_num, week_key))
+        cur.execute('''SELECT id FROM Weeks where week = (?)''', (week_num,))
+
+        week_id = cur.fetchone()[0]
+
+        #adds information into unemployment rates table
+        cur.execute('''INSERT OR IGNORE INTO weeks (week, initial_nsa_claims) VALUES (?, ?, ?)''', (week_id, unemployment_claim_num, total))
+    
+    print("Finished adding data")
+    conn.commit()
+
+
+
+
 
 
 def main():
-    """Takes nothing as an input and returns nothing. Calls the functions setUpDatabase(), set_up_tables(), set_up_artist_id_table(), fill_up_hot_100_table(), and write_data_to_file(). Closes the database connection."""
-    print(make_dict())
+    """Takes nothing as an input and returns nothing. Calls the functions make_dict(), get_weekly_cumlative_total(), create_database(), and fill_database(). Closes the database connection."""
+
+    weekly_umemployment_claims_dict = make_dict()
+    cumulative_claims_list = get_weekly_cumulative_total()
+
+    cur, conn = create_database('unemployment_tables.db')
+    fill_database(weekly_umemployment_claims_dict, cumulative_claims_list)
+
+    conn.close()
+
 
 if __name__ == "__main__":
     main()
